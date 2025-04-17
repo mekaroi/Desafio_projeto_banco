aedcbcc883527dab28c5e85d1e5a7bea5d178264
Igor Mekaro <iguinhoo.msn@hotmail.com>
2025-04-17 18:20:54 -0300

    Adicionando Pacote Desfio banco

---
+++ Desafio_projeto_banco/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="Desafio_projeto_banco",
+    version="1.04.25.0",
+    py_modules=['Desafio_projeto_banco'],
+    author="Igor Mekaro",
+    author_email="iguinhoo.msn@hotmail.com",
+    description="É um projeto de banco, um desafio proposto pela DIO.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+)
